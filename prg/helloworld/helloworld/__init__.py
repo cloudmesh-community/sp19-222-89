9b5f519f353b633c2dba4e780b4185c8fb551ecc
Brandon Fischer <brandon@DESKTOP-88Q79AK.localdomain>
2019-02-12 15:08:29 -0500

    Added helloworld and prime examples. Created a short writeup on making python packages/using them

---
+++ prg/helloworld/helloworld/__init__.py
@@ -0,0 +1,2 @@
+from helloworld import hello
+from joke import our_joke
---
+++ prg/helloworld/setup.py
@@ -0,0 +1,4 @@
+#things that go here include: name, email, readme, very minimum definitions for file structure
+from setuptools import setup
+setup(name='helloworld', version='1.0.0', url='', author='Brandon Fischer', packages=[''])
+ 
