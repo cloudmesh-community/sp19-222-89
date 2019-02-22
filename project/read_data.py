6cab059b1d25ff1f8c6f38a159e3cf903722438d
Admin <admin@149-161-192-190.dhcp-bl.indiana.edu>
2019-02-21 20:40:07 -0500

    Wrote a bunch of python code, currenty stuck at passing in input to create the model because it says: Input contains NaN, infinity, or a value too large for dtype float64

--- project/read_data.py
+++ project/read_data.py
@@ -1,6 +1,25 @@
-import csv
+#this function is meant to read in the data from the .csv file and return
+#a numpy array containing all the data
 
+"""Important note: when the data is read using the csv.DictReader,
+the order of the features is changed from the original. In the 
+list of dictionaries, the order of features is as follows:
+ISOS_Size_x, ISOS_Size_y, COST_Size_y, COST_Size_x, Coord_y, Coord_x,
+ISOS_z, COST_z, Cone_Type"""
+
+import csv
 def read(fpath):
-	with open(fpath) as csvfile:
-		readCSV = csv.reader(csvfile, delimiter=',')
-		print(readCSV)
+
+    #make file object
+    with open(fpath) as f_obj:
+
+        #make reader object
+        reader = csv.DictReader(f_obj, delimiter=',')
+
+        #make data list
+        data = []
+
+        for row in reader:
+            data.append(row)
+
+        return data
