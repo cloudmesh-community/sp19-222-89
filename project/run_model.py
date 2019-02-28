e1343dacfd3444551878cb7ffcc8c4dc2d192725
rdanehy <rdanehy@iu.edu>
2019-02-28 18:31:23 -0500

    Generalization part 2

--- project/run_model.py
+++ project/run_model.py
@@ -19,6 +19,34 @@ def run_model(fpath):
     #convert our data list to a numpy array
    b = np.array(list_input)
 
+   #randomly shuffle the data/corresponding labels
+    #np.random.shuffle(a);
+    
+    #separate features from labels
+
+    #find out how many labels we need
+   b_shape = np.shape(b)
+    
+    #make a new array for it
+   b_labels = np.zeros(b_shape[0])
+    
+    #populate that array (slice isn't working so)
+   for i in range(0, b_shape[0]):
+      if(list(b[i])[8] == '3'):
+         b_labels[i] = 1
+      else:
+         b_labels[i] = 0
+
+
+    #now make array for features
+   b_features = np.zeros((b_shape[0], nfeatures))
+
+    
+    #populate that array
+   for i in range(0, b_shape[0]):
+      for j in range(0, nfeatures):
+         b_features[i][j] = list(b[i])[j]
+
    print(b)
    anwser = svm_model.predict(b) #Using test data to make prediction and validate the model
    return anwser 
