9a1b5448070ba23d2830f0ad8cce76b3eef1d637
Admin <admin@149-161-210-93.dhcp-bl.indiana.edu>
2019-03-07 15:37:23 -0500

    Changed project directory structure.

---
+++ project/app/app/__init__.py
@@ -0,0 +1,4 @@
+from read_data import read
+from split_data import split, format
+from run_model import run_model
+from test_model import read_model_from_file, run_metrics_model, F1, run_metrics_test
---
+++ project/app/app/test_model.py
@@ -0,0 +1,53 @@
+#this file will test the model we generated and report a number of
+#metrics to help determine the quality of the model
+
+from joblib import load
+from copy import copy
+
+#read the model from a saved file so we don't generate a new one each time
+def read_model_from_file():
+    svm_model = load('model.joblib')
+    return svm_model
+
+#run metrics on a newly generated/updated model
+def run_metrics_model(model, features_testing, labels_testing):
+    TP, FP, TN, FN = 0, 0, 0, 0
+
+    for n in range(0, len(labels_testing)):
+        
+        mp = model.predict(features_testing)
+        if mp[n] == labels_testing[n] and mp[n] == 1:
+            TP += 1
+        if mp[n] == labels_testing[n] and mp[n] == 0:
+            TN += 1
+        if mp[n] != labels_testing[n] and mp[n] == 1:
+            FP += 1
+        if mp[n] != labels_testing[n] and mp[n] == 0:
+            FN += 1
+    print("TP:", TP , 'TN:',  TN, 'FP:', FP, 'FN', FN)
+
+    return TP, FP, TN, FN
+
+#reports F1 score given TP FP TN FN
+def F1(TP, FP, TN, FN):
+    return ((2 * TP)/(2*TP + FP + FN))
+
+#runs metrics on the predictions made by an existing model
+#Different from run_metrics_model because we don't pass in the features_testing
+#and features_training, but instead we pass in already made predictions
+#and truth values
+def run_metrics_test(predictions, truth_values):
+    TP, FP, TN, FN = 0, 0, 0, 0
+
+    for n in range(0, len(truth_values)):
+        
+        if predictions[n] == truth_values[n] and predictions[n] == 1:
+            TP += 1
+        if predictions[n] == truth_values[n] and predictions[n] == 0:
+            TN += 1
+        if predictions[n] != truth_values[n] and predictions[n] == 1:
+            FP += 1
+        if predictions[n] != truth_values[n] and predictions[n] == 0:
+            FN += 1
+
+    return TP, FP, TN, FN
